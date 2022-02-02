8eddaf71db9b8cc0ef75244f30092a9953661302
moi <moi@localhost.localdomain>
2022-02-02 16:15:05 +0000

    added npm run build in package.json and put /dist in gitignore

--- dist/test/test-projects/no-config/hardhat.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../../../src/index");
-const config = {
-    solidity: "0.7.3",
-};
-exports.default = config;
-//# sourceMappingURL=hardhat.config.js.map
